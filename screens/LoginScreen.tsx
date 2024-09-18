11994313acb01299fef12b316a9e59992ca9d9f6
Avoracity <avoracity7@gmail.com>
2024-09-18 18:01:39 -0400

    cat image and lividi title

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,6 +1,6 @@
 // screens/LoginScreen.tsx
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image} from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'; // Import NativeStackNavigationProp
 
 type Props = {
@@ -12,7 +12,11 @@ type Props = {
 const LoginScreen: React.FC<Props> = ({ navigation }) => {
   return (
     <View style={styles.container}>
+      <Image source={require('../assets/images/Cat_Body.png')} style={styles.catBody}/>
+      <Image source={require('../assets/images/Cat_Paw.png')} style={styles.catPaw}/>
       <View style={styles.loginContainer}>
+      <Image source={require('../assets/images/Lividi_Title.png')} style={styles.lividiTitle}/>
+  
       <Text>Login Screen</Text>
       <Button
         title="Go to Home"
@@ -38,8 +42,26 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 30,
     alignItems: 'center',
     justifyContent: 'center',
-   
-  }
+    zIndex: 1, // level of layering
+  }, 
+  
+  catPaw: {
+    marginLeft: '14%',
+    top: '3%',
+    height : 30,
+    width: 30,
+    zIndex: 2,
+    
+  },
+  catBody: {
+    marginLeft: '60%',
+    top: '15%',
+    zIndex:0,
+  },
+  lividiTitle: {
+    bottom: '20%',
+  },
+  
 });
 
 export default LoginScreen;
